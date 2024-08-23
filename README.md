d3b3369d41e0a2fc9d082bb7a11e8a7daf1adf8c
rbi_international <rohit.bharti8211@gmail.com>
2024-08-23 14:59:31 +0530

    folder structure created

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # resume-evaluation-system
+
+# How to run?
+
+### STEPS:
+
+Clone the repository
+
+```bash
+Project repo: https://github.com/rbi-international/resume-evaluation-system.git --- here use your repo name instead
+```
+### STEP 01- Create a conda environment after opening the repository
+
+```bash
+conda create -p venv python=3.9 -y
+```
+
+```bash
+source activate ./venv
+```
+
+
+### STEP 02- install the requirements
+```bash
+pip install -r requirements.txt
+```
+
+
+### Create a `.env` file in the root directory and add your OPENAI_API_KEY credentials as follows:
+
+```ini
+OPENAI_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+```
+
+
+```bash
+# Finally run the following command
+python app.py
+```
+
+Now,
+```bash
+open up localhost:
+```
+
+
+### Techstack Used:
+
+- pandas
+- nltk
+- PyMuPDF
+- python-docx
+- sklearn
+
---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+list_of_files = [
+    "data/raw_resumes/",
+    "data/processed_data/",
+    "models/trained_model/",
+    "models/evaluation/",
+    "src/data_preprocessing.py",
+    "src/feature_extraction.py",
+    "src/model_training.py",
+    "src/predict.py",
+    "src/dynamic_weighting.py",
+    "notebooks/EDA.ipynb",
+    "requirements.txt"
+    ".env",
+    "requirements.txt",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb"
+]
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
